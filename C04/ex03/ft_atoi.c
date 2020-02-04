f8d00e4e8ad0df3ed363a5793fca0ce0a84d31d5
Seyong Oh <seoh@c1r4s6.42seoul.kr>
2020-02-04 13:13:45 +0900

    added Exersises

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,35 +6,34 @@
 /*   By: seoh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 05:49:15 by seoh              #+#    #+#             */
-/*   Updated: 2020/02/03 19:50:40 by seoh             ###   ########.fr       */
+/*   Updated: 2020/02/04 13:12:17 by seoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int sign_check(int sign_count)
+int	sign_check(int sign_count)
 {
-	if(sign_count % 2 == 1)
+	if (sign_count % 2 == 1)
 		return (-1);
 	return (1);
 }
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
 	int sign_count;
 	int value;
 
+	sign_count = 0;
 	value = 0;
-	while (*str == '\t' || *str == '\n' || *str == '\v' ||
-			*str == '\f' || *str == '\r' ||  *str == ' ')
-			str++;
+	while (*str == '\t' || *str == '\n' || *str == '\v'
+			|| *str == '\f' || *str == '\r' || *str == ' ')
+		str++;
 	while (*str == '-' || *str == '+')
 	{
 		str++;
-		if(*str == '-')
+		if (*str == '-')
 			sign_count++;
 	}
-	while(*str >= '0' && *str <= '9')
+	while (*str >= '0' && *str <= '9')
 	{
 		value = value * 10;
 		value = value + *str - 48;
